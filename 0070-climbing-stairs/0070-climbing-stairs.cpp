100fe5f25b0121162c718de131aa3cbfbe14b7b6
Ham0nta <155809487+Ham0nta@users.noreply.github.com>
2026-08-01 17:41:57 +0600

    Time: 0 ms (100%), Space: 8.7 MB (20.66%) - LeetHub - Time: O(n), Space: O(n)

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+    int dp(int n,vector<int>& v){
+        if(n<=1) return 1;
+        if(v[n]!=-1) return v[n];
+        return v[n]=dp(n-1,v)+dp(n-2,v);
+    }
     int climbStairs(int n) {
-        int dp[n+1];
-        dp[0]=1,dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        vector<int>v(n+1,-1);
+        return dp(n,v);
     }
 };
 
